37164a2bba5591bb75b8f1716b57cb24ce0ecd7d
Maksim Wojtk <wojtk.maksim@gmail.com>
2023-11-20 17:41:56 +0300

    Apply 7_2_HW6_mail_history patch

--- sql/lb_apply.bat
+++ sql/lb_apply.bat
@@ -1,8 +1,8 @@
-set LB_HOME=c:\java\liquibase-3.5.3
+set LB_HOME="c:\Program Files\liquibase"
 call %LB_HOME%\liquibase.bat --driver=org.postgresql.Driver ^
 --classpath=%LB_HOME%\lib ^
 --changeLogFile=databaseChangeLog.sql ^
 --url="jdbc:postgresql://localhost:5432/masterjava" ^
---username=user ^
+--username=postgres ^
 --password=password ^
 migrate
